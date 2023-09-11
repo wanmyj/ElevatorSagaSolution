1cd1a931e5104f56f17ee46440d7c00f2aee5732
wanmyj <41611679+wanmyj@users.noreply.github.com>
2023-09-11 18:03:46 +0800

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -27,7 +27,7 @@
         elevator.destinationQueue.sort(function(a, b){return b-a});
       }
     }
-    
+
     let printInfo = function printArgs(...args) {
       printInfo.count = (printInfo.count || 0) + 1;
       console.log(printInfo.count);
@@ -52,71 +52,164 @@
       console.log(floorNums.join(' '));
     };
 
-    function getDirectionAtFloor(elevator, floor) {
+    // 这个函数应该在————————————时候被call
+    function getDirectionWhenPassFloor(elevator, floor) {
       const floorNum = floor.floorNum();
       const queue = elevator.destinationQueue();
-  
+
       if (queue.includes(floorNum)) {
-          // 如果目标楼层在队列中
-          const nextFloor = queue[queue.indexOf(floorNum) + 1];
-          if (nextFloor > floorNum) {return "up";} 
-          else { return "down"; }
+        // 如果目标楼层在队列中
+        if (queue.indexOf(floorNum) === queue.length - 1) { return "idel"};
+        const nextFloor = queue[queue.indexOf(floorNum) + 1];
+        if (nextFloor > floorNum) { return "up"; }
+        else { return "down"; }
       } else {
-          // 如果目标楼层不在队列中
-          if (queue.length === 0) {
-              // 如果队列为空，电梯当前处于空闲状态 TODO: getEvent
-              return floor.event
+        // 如果目标楼层不在队列中
+        if (queue.length === 0) {
+          // 如果队列为空，电梯当前处于空闲状态
+          return "idel";
+        } else {
+          // 队列不为空
+          // 如果floorNum大于等于queue的最大值，返回下，或者小于等于queue的最小值，则返回上
+          // 如果queue里的数字小于queue的最大值且大于最小值，则返回第一次经过此floorNum时的方向 
+          // 比如队列是 [1,2,4], floorNum 是3, 返回up
+          // 比如队列是 [3,5,2], floorNum 是4, 返回up
+          // 比如队列是 [3,5,1], floorNum 是2, 返回down
+          const maxQueueFloor = Math.max(...queue);
+          const minQueueFloor = Math.min(...queue);
+
+          if (floorNum >= maxQueueFloor) {
+            return "down";
+          } else if (floorNum <= minQueueFloor) {
+            return "up";
           } else {
-              // 队列不为空
-              // 如果floorNum大于等于queue的最大值，返回下，或者小于等于queue的最小值，则返回上
-              // 如果queue里的数字小于queue的最大值且大于最小值，则返回第一次经过此floorNum时的方向 
-              // 比如队列是 [1,2,4], floorNum 是3, 返回up
-              // 比如队列是 [3,5,2], floorNum 是4, 返回up
-              // 比如队列是 [3,5,1], floorNum 是2, 返回down
-              const maxQueueFloor = Math.max(...queue);
-              const minQueueFloor = Math.min(...queue);
-          
-              if (floorNum >= maxQueueFloor) {
-                  return "down";
-              } else if (floorNum <= minQueueFloor) {
-                  return "up";
-              } else {
-                  for (let i = 0; i < queue.length - 1; i++) {
-                      if ((queue[i] > floorNum && queue[i + 1] < floorNum) ||
-                          (queue[i] < floorNum && queue[i + 1] > floorNum)) {
-                          return queue[i] > floorNum ? "down" : "up";
-                      }
-                  }
+            for (let i = 0; i < queue.length - 1; i++) {
+              if ((queue[i] > floorNum && queue[i + 1] < floorNum) ||
+                (queue[i] < floorNum && queue[i + 1] > floorNum)) {
+                return queue[i] > floorNum ? "down" : "up";
               }
+            }
           }
-      }
-  }
-    floors.forEach(function (floor) {
-      floor.on("up_button_pressed down_button_pressed", function () {
-        // 如果在某个电梯的queue里，且方向一致，由这个电梯来接，如果有多个，则找最近
-        let mergedQueue = new Set();
-        elevators.forEach(elevator => {
-          // 遍历每个电梯并将其目标楼层添加到合并队列中
-            const destinationQueue = elevator.destinationQueue();
-            destinationQueue.forEach(floor => {
-                mergedQueue.add(floor);
-            });
-        });
-        // 如果它比某部电梯的queue最大值都大，且此电梯向上运动，找出最近的一部电梯，它来接
-        // 如果它比某部电梯的queue最小值都小，且此电梯向下运动，找出最近的一部电梯，它来接
-        // 如果上下两层之内有电梯即将到达（路过电梯），而且满载率小于0.7，则由它来接乘客
-        // 如果在某个电梯的queue里，由这个电梯来接
-        // 否则找个空的，find an idle elevator if possible，走idleArrange的处理
-
-        if (mergedQueue.has(desiredNumber)) return;
-
-        printInfo("Floor Button Pressed on " + floor.floorNum());
-        let choice = findIdle(floor.floorNum());
-        if (choice.length) {
-          choice[0].goToFloor(floor.floorNum());
         }
-        // 如果也没有空的，找一个没有满载，且最近方向一致的到达，来接
+      }
+    };
+
+    function findElevatorsStoppedAtFloor(floor, recorder) {
+      let foundElevators = elevators.filter((elevator) => (elevator.destinationQueue.includes(floor.floorNum())));
+      if (foundElevators.count === 0) return foundElevators;
+
+      foundElevators = foundElevators.filter((elevator) => {
+        let dir = getDirectionWhenPassFloor(elevator, floor);
+        if (dir == "idel") { return true; }
+        if (dir == "up" && recorder["up"]) {return true; }
+        if (dir == "down" && recorder["down"]) {return true; }
+        return false;
+      })
+    }
+    
+    floors.forEach(function (floor) {
+      let recorder = {
+        "up": false,
+        "down": false
+      }
+      floor.on("up_button_pressed", function () {
+        recorder["up"] = true;
       });
+      floor.on("down_button_pressed", function () {
+        recorder["down"] = true;
+      });
+
+      // 如果在某个电梯的queue里，且方向一致，由这个电梯来接，如果有多个，则找满载率最小*距离最近的
+      let elevatorsWithThisFloor = findElevatorsStoppedAtFloor(floor, recorder);
+      if (elevatorsWithThisFloor.length > 0) {
+        return elevatorsWithThisFloor.reduce((bestElevator, elevator) => {
+          let loadFactor1 = bestElevator.loadFactor();
+          let loadFactor2 = elevator.loadFactor();
+          let distance1 = distance(bestElevator, floor.floorNum());
+          let distance2 = distance(elevator, floor.floorNum());
+          let value1 = loadFactor1 * distance1;
+          let value2 = loadFactor2 * distance2;
+      
+          if (value1 < value2) {
+            return bestElevator;
+          } else {
+            return elevator;
+          }
+        })
+      }
+
+      // 如果它比某部电梯的queue最大值都大，且此电梯向上运动，找出最近的一部电梯，它来接
+      // 如果它比某部电梯的queue最小值都小，且此电梯向下运动，找出最近的一部电梯，它来接
+      function findElevatorWithMinDistance(elevators, targetNumber) {
+        const satisfiedElevators = new Map();
+      
+        for (const elevator of elevators) {
+          const destinationQueue = elevator.destinationQueue;
+          if (destinationQueue.length > 0) {
+            const maxQueueValue = Math.max(...destinationQueue);
+            const minQueueValue = Math.min(...destinationQueue);
+      
+            // Check if the elevator's queue meets the conditions
+            if (maxQueueValue < targetNumber || minQueueValue > targetNumber) {
+              const distance = Math.abs(maxQueueValue - targetNumber);
+              satisfiedElevators.set(elevator, distance);
+            }
+          }
+        }
+      
+        if (satisfiedElevators.size === 0) {
+          return null; // No elevators satisfy the conditions
+        }
+      
+        // Sort the satisfied elevators by distance and then by load factor
+        const sortedElevators = Array.from(satisfiedElevators.keys()).sort((elevator1, elevator2) => {
+          const distance1 = satisfiedElevators.get(elevator1);
+          const distance2 = satisfiedElevators.get(elevator2);
+          if (distance1 !== distance2) {
+            return distance1 - distance2;
+          } else {
+            return elevator1.loadFactor() - elevator2.loadFactor();
+          }
+        });
+      
+        // Return the elevator with the minimum distance and, if there are ties, the minimum load factor
+        return sortedElevators[0];
+      }
+      // 如果上下两层之内有电梯即将到达（路过电梯），而且满载率小于0.7，则由它来接乘客
+      elevators.forEach(elevator => {
+        if (elevator.loadFactor() <= 0.7) {
+          onFloorNum = elevator.currentFloor();
+          if (onFloorNum >= floorNum && onFloorNum <= floorNum + 2 && getDirectionWhenPassFloor(elevator, floor) != "down") { return elevator;}
+          if (onFloorNum <= floorNum && onFloorNum >= floorNum - 2 && getDirectionWhenPassFloor(elevator, floor) != "up") { return elevator;}
+        }
+      })
+      // 否则找个空的，find an idle elevator if possible，
+      let choice = findIdle(floor.floorNum());
+      if (choice.length) {
+        choice[0].goToFloor(floor.floorNum());
+      }
+      // 如果在某个电梯的queue里，由这个电梯来接
+      elevators.forEach(elevator => {
+        if (elevator.destinationQueue().includes(floorNum)) {
+          elevator.destinationQueue.push(floorNum);
+          sortQueue(elevator);
+          elevator.checkDestinationQueue();
+        }
+      })
+      // 如果也没有空的，找一个没有满载，且方向一致的最近到达，来接
+
+      let mergedQueue = new Set();
+      elevators.forEach(elevator => {
+        // 遍历每个电梯并将其目标楼层添加到合并队列中
+          const destinationQueue = elevator.destinationQueue();
+          destinationQueue.forEach(floor => {
+              mergedQueue.add(floor);
+          });
+      });
+      if (mergedQueue.has(desiredNumber)) return;
+
+      printInfo("Floor Button Pressed on " + floor.floorNum());
+
     });
 
     elevators.forEach(function (elevator, index) {
@@ -169,7 +262,7 @@
 
       });
       elevator.on("stopped_at_floor", function (floorNum) {
-        // 只需要表明上下就好，note：电梯只有在方向一致的时候才会停
+        // 只需要表明上下就好，note：如果没有在电梯队列里，电梯只有在方向一致的时候才会停
         // 通过queue的下一站来确定是上还是下。
         // 如果queue为空，则按照TODO逻辑选方向
         // 如果向上有电梯来接，则只显示向下
